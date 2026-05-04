7e1e3005f1a17aade5669e6db9ee29edbb3d72ba
Aaron <33184112+astark929@users.noreply.github.com>
2026-05-03 21:54:39 -0700

    Update final.ino

--- final.ino
+++ final.ino
@@ -2,34 +2,35 @@
 #include "move.h"
 
 move m;
+Servo s;
 
-const int ENA = 11;
-const int ENB = 12;
-const int ENC = 9;
-const int END = 10;
 
-const int IN1 = 5;
-const int IN2 = 6;
-
-const int IN3 = 7;
-const int IN4 = 8;
-
-const int IN5 = 22;
-const int IN6 = 24;
-
-const int IN7 = 26;
-const int IN8 = 28;
-
-const int irLeft  = 36;
-const int irRight = 37;
 
 int speedPWM = 75;
+
+enum AvoidState {
+  CHOOSE_DIR,
+  SLIDE,
+  FORWARD_CLEAR,
+  SCAN_EDGE,
+  RETURN_PATH
+};
+enum RobotState {
+  TRACKING,
+  AVOIDING
+};
+
+AvoidState avoidState = CHOOSE_DIR;
+int avoidDir = 0; // -1 = left, +1 = right
+
+RobotState state = TRACKING;
 
 void setup() {
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
   pinMode(ENC, OUTPUT);
   pinMode(END, OUTPUT);
+  //speed control pins
 
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -39,86 +40,46 @@ void setup() {
   pinMode(IN6, OUTPUT);
   pinMode(IN7, OUTPUT);
   pinMode(IN8, OUTPUT);
+  //motor pins
 
   pinMode(irLeft,  INPUT);
   pinMode(irRight, INPUT);
+  //lidar pins
+
+  pinMode(Trig_PIN, OUTPUT);
+  pinMode(Echo_PIN, INPUT);
+  //ultrasonic pins
+
+  s.attach(SERVO_PIN);
+  //servo pin
+
+  s.write(90); // center
 
   Serial.begin(9600);
 }
 
-void moveForward() {
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-  digitalWrite(IN5, HIGH); digitalWrite(IN6, LOW);
-  digitalWrite(IN7, HIGH); digitalWrite(IN8, LOW);
-
-  analogWrite(ENA, speedPWM);
-  analogWrite(ENB, speedPWM);
-  analogWrite(ENC, speedPWM);
-  analogWrite(END, speedPWM);
-}
-
-void moveBackward() {
-  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
-  digitalWrite(IN5, LOW); digitalWrite(IN6, HIGH);
-  digitalWrite(IN7, LOW); digitalWrite(IN8, HIGH);
-
-  analogWrite(ENA, speedPWM);
-  analogWrite(ENB, speedPWM);
-  analogWrite(ENC, speedPWM);
-  analogWrite(END, speedPWM);
-}
-
-void moveLeft() {
-  digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-  digitalWrite(IN5, HIGH); digitalWrite(IN6, LOW);
-  digitalWrite(IN7, LOW);  digitalWrite(IN8, HIGH);
-
-  analogWrite(ENA, speedPWM);
-  analogWrite(ENB, speedPWM);
-  analogWrite(ENC, speedPWM);
-  analogWrite(END, speedPWM);
-}
-
-void stopCar() {
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
-  analogWrite(ENC, 0);
-  analogWrite(END, 0);
-
-  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
-  digitalWrite(IN5, LOW); digitalWrite(IN6, LOW);
-  digitalWrite(IN7, LOW); digitalWrite(IN8, LOW);
-}
-
 void loop() {
-  int left  = digitalRead(irLeft);
-  int right = digitalRead(irRight);
+  // /*
+  if(digitalRead(LeftObstacleSensor) == low || digitalRead(RightObstacleSensor) == low){
+    //detects object with lidar 
 
-  if (left == LOW && right == LOW) {
-    stopCar();
-    delay(200);
-
-    moveBackward();
-    delay( 500);
-
-    stopCar();
-    delay(200);
-
-    moveLeft();
-    delay(1000);
-
-    stopCar();
-    delay(200);
-
+    Serial.println("object detected");
+    avoidObstacle();
+    //m.stop();
   }
-  else {
+  else{
+    Serial.println("object not detected");
     tracking();
   }
+  //*/
+  //tracking();
+  //avoidObstacle();
+  //moveRight();
+  //m.Forward(100);
+  //m.turn(-100, 100);
 }
+
+
 int watch(){
   long echo_distance;
 
@@ -215,6 +176,117 @@ void tracking()
      // m.stop();     
  }
 }
+
+bool obstacleDetected() {
+  int dist = watch();  // ultrasonic
+
+  int leftLidar  = digitalRead(LeftObstacleSensor);
+  int rightLidar = digitalRead(RightObstacleSensor);
+
+  if (dist < 10) return true;
+
+  if (leftLidar == LOW || rightLidar == LOW) return true;
+
+  return false;
+}
+
+bool lineDetected() {
+  int s0 = !digitalRead(sensor1);
+  int s1 = !digitalRead(sensor2);
+  int s2 = !digitalRead(sensor3);
+  int s3 = !digitalRead(sensor4);
+  int s4 = !digitalRead(sensor5);
+
+  return (s0 || s1 || s2 || s3 || s4);
+}
+
+void avoidObstacle() {
+
+  int dist = watch();
+  int leftLidar  = digitalRead(LeftObstacleSensor);
+  int rightLidar = digitalRead(RightObstacleSensor);
+
+  switch (avoidState) {
+
+    // 1. Decide direction
+    case CHOOSE_DIR:
+      if (leftLidar == LOW) {
+        avoidDir = 1; // go right
+      } 
+      else {
+        avoidDir = -1; // go left
+      }
+      avoidState = SLIDE;
+      break;
+
+    // 2. Move sideways until clear
+    case SLIDE:
+      if (dist < distancelimit || leftLidar == LOW || rightLidar == LOW) {
+        if (avoidDir == -1) moveLeft();
+        else moveRight();
+      } 
+      else {
+        stopCar();
+        delay(100);
+        avoidState = FORWARD_CLEAR;
+      }
+      break;
+
+    // 3. Move forward past object
+    case FORWARD_CLEAR:
+      moveForward();
+      delay(100);
+      stopCar();
+
+      // turn ultrasonic toward object
+      if (avoidDir == -1) {
+        s.write(150); // looking right
+      } 
+      else {
+        s.write(30); // looking left
+      }
+      avoidState = SCAN_EDGE;
+      break;
+
+    // 4. Follow object edge
+    case SCAN_EDGE:
+      if (watch() < distancelimit) {
+        moveForward();
+      } 
+      else {
+        stopCar();
+        avoidState = RETURN_PATH;
+        moveForward();
+        delay(200);
+        stopCar();
+      }
+      break;
+
+    // 5. Move back toward original path
+    case RETURN_PATH:
+      if (avoidDir == -1) moveRight();
+      else moveLeft();
+
+      delay(400);
+      stopCar();
+
+      avoidState = CHOOSE_DIR;
+      state = TRACKING; // hand control back
+      break;
+  }
+}
+
+bool obstacleAhead(){
+  int distance = watch();
+  int lidarleft = digitalRead(RightObstacleSensor);
+  int lidarright = digitalRead(LeftObstacleSensor);
+
+  if(distance < distancelimit || lidarleft == LOW || lidarright == LOW){
+    return true;
+  }
+  return false;
+}
+
 void sharpRightTurn(int speed_left,int speed_right)
 {
   m.Motor_FL(speed_left);
@@ -253,4 +325,64 @@ void reverse (int speed)
   m.Motor_BL(-1 * speed);
   m.Motor_FR(-1 * speed);
   m.Motor_BR(-1 * speed); 
+}
+
+void moveForward() {
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  digitalWrite(IN5, HIGH); digitalWrite(IN6, LOW);
+  digitalWrite(IN7, HIGH); digitalWrite(IN8, LOW);
+
+  analogWrite(ENA, speedPWM);
+  analogWrite(ENB, speedPWM);
+  analogWrite(ENC, speedPWM);
+  analogWrite(END, speedPWM);
+}
+
+void moveBackward() {
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
+  digitalWrite(IN5, LOW); digitalWrite(IN6, HIGH);
+  digitalWrite(IN7, LOW); digitalWrite(IN8, HIGH);
+
+  analogWrite(ENA, speedPWM);
+  analogWrite(ENB, speedPWM);
+  analogWrite(ENC, speedPWM);
+  analogWrite(END, speedPWM);
+}
+
+void moveLeft() {
+  digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  digitalWrite(IN5, HIGH); digitalWrite(IN6, LOW);
+  digitalWrite(IN7, LOW);  digitalWrite(IN8, HIGH);
+
+  analogWrite(ENA, speedPWM);
+  analogWrite(ENB, speedPWM);
+  analogWrite(ENC, speedPWM);
+  analogWrite(END, speedPWM);
+}
+
+void moveRight() {
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
+  digitalWrite(IN5, LOW);  digitalWrite(IN6, HIGH);
+  digitalWrite(IN7, HIGH); digitalWrite(IN8, LOW);
+
+  analogWrite(ENA, speedPWM);
+  analogWrite(ENB, speedPWM);
+  analogWrite(ENC, speedPWM);
+  analogWrite(END, speedPWM);
+}
+
+void stopCar() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  analogWrite(ENC, 0);
+  analogWrite(END, 0);
+
+  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
+  digitalWrite(IN5, LOW); digitalWrite(IN6, LOW);
+  digitalWrite(IN7, LOW); digitalWrite(IN8, LOW);
 }
